eacad3bc23128244949f75b18cf9e0d71caa66f0
Shashank Kumar <shashankrust@gmail.com>
2022-03-24 17:20:47 +0530

    share preference

--- app/src/main/java/com/example/persistentdata/MainActivity.java
+++ app/src/main/java/com/example/persistentdata/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.persistentdata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final String TAG = MainActivity.class.getSimpleName();
+    private int mCounter = 0;
+    private TextView textView;
+    private final String KEY = "key";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textview);
+        mCounter = getPreferences(MODE_PRIVATE).getInt(KEY, 0);
+        updateDate();
+
+
+        findViewById(R.id.button).setOnClickListener(view->{
+            mCounter++;
+            updateDate();
+        });
+    }
+
+    private void updateDate() {
+        textView.setText(String.valueOf(mCounter));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(KEY, mCounter);
+        editor.apply();
     }
 }
